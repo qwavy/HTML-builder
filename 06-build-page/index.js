c2f356f3fa794f90061788edb59fb31fef4cd5ab
Nursultan <97668069+qwavy@users.noreply.github.com>
2025-01-19 21:53:28 +0500

    refactor: some refactor 06

--- 06-build-page/index.js
+++ 06-build-page/index.js
@@ -116,45 +116,32 @@ const rr = fs.createReadStream(path.join(__dirname, 'template.html'));
 let htmlData = ""
 
 
-collectingData();
-async function createIndexHTML(data) {
-  fs.writeFile(
-    path.join(__dirname, 'project-dist', 'index.html'),
-    data,
-    (err) => {
-      if (err) {
-        console.log(err);
-      }
-    },
-  );
-}
+rr.on('data', (chunk) => {
+  htmlData = chunk.toString();
+});
 
-async function collectingData() {
-  rr.on('data', (chunk) => {
-    htmlData = chunk.toString();
-  });
-
-  fs.readdir(path.join(__dirname, 'components'), (err, files) => {
-    if (err) {
-      console.log(err);
-      return;
-    }
-    getDataFromComponents(files);
-  });
-}
-
-async function getDataFromComponents(files) {
-  try {
-    files.forEach((file) => {
-      const rrFile = fs.createReadStream(
-        path.join(__dirname, 'components', file),
-      );
-      rrFile.on('data', (chunk) => {
-        htmlData = htmlData.replace(`{{${path.basename(file, '.html')}}}`, chunk.toString())
-          createIndexHTML(htmlData)
-      });
-    });
-  } catch (e) {
-    throw e;
+fs.readdir(path.join(__dirname, 'components'), (err, files) => {
+  if (err) {
+    console.log(err);
+    return;
   }
-}
+  files.forEach((file) => {
+    const rrFile = fs.createReadStream(
+        path.join(__dirname, 'components', file),
+    );
+    rrFile.on('data', (chunk) => {
+      htmlData = htmlData.replace(`{{${path.basename(file, '.html')}}}`, chunk.toString())
+
+      fs.writeFile(
+          path.join(__dirname, 'project-dist', 'index.html'),
+          htmlData,
+          (err) => {
+            if (err) {
+              console.log(err);
+            }
+          },
+      );
+    });
+  });
+});
+
